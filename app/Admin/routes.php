dbc31719201cc9009f433017245792bcb091b7b3
vagrant <vagrant@homestead>
2018-07-26 06:16:52 +0000

    管理后台 - 订单列表

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -11,9 +11,12 @@ Route::group([
 ], function (Router $router) {
     $router->get('/', 'HomeController@index');
     $router->get('users', 'UsersController@index');
+
     $router->get('products', 'ProductsController@index');
     $router->get('products/create', 'ProductsController@create');
     $router->post('products', 'ProductsController@store');
     $router->get('products/{id}/edit', 'ProductsController@edit');
     $router->put('products/{id}', 'ProductsController@update');
+
+    $router->get('orders', 'OrdersController@index')->name('admin.orders.index');
 });
