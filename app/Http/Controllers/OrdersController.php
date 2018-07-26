f9be7a2f95f2085079bda99880c0acc38c43437d
vagrant <vagrant@homestead>
2018-07-26 07:23:41 +0000

    评价商品

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -7,68 +7,13 @@ use App\Models\UserAddress;
 use App\Models\Order;
 use Illuminate\Http\Request;
 use App\Services\OrderService;
+use Carbon\Carbon;
+use App\Http\Requests\SendReviewRequest;
+use App\Events\OrderReviewd;
 
 class OrdersController extends Controller
 {
-    // 利用 Laravel 的自动解析功能注入 CartService 类
-//    public function store(OrderRequest $request, CartService $cartService)
-//    {
-//        $user  = $request->user();
-//        // 开启一个数据库事务
-//        $order = \DB::transaction(function () use ($user, $request, $cartService) {
-//            $address = UserAddress::find($request->input('address_id'));
-//            // 更新此地址的最后使用时间
-//            $address->update(['last_used_at' => Carbon::now()]);
-//            // 创建一个订单
-//            $order   = new Order([
-//                'address'      => [ // 将地址信息放入订单中
-//                    'address'       => $address->full_address,
-//                    'zip'           => $address->zip,
-//                    'contact_name'  => $address->contact_name,
-//                    'contact_phone' => $address->contact_phone,
-//                ],
-//                'remark'       => $request->input('remark'),
-//                'total_amount' => 0,
-//            ]);
-//            // 订单关联到当前用户
-//            $order->user()->associate($user);
-//            // 写入数据库
-//            $order->save();
-//
-//            $totalAmount = 0;
-//            $items       = $request->input('items');
-//            // 遍历用户提交的 SKU
-//            foreach ($items as $data) {
-//                $sku  = ProductSku::find($data['sku_id']);
-//                // 创建一个 OrderItem 并直接与当前订单关联
-//                $item = $order->items()->make([
-//                    'amount' => $data['amount'],
-//                    'price'  => $sku->price,
-//                ]);
-//                $item->product()->associate($sku->product_id);
-//                $item->productSku()->associate($sku);
-//                $item->save();
-//                $totalAmount += $sku->price * $data['amount'];
-//                if ($sku->decreaseStock($data['amount']) <= 0) {
-//                    throw new InvalidRequestException('该商品库存不足');
-//                }
-//
-//            }
-//
-//            // 更新订单总金额
-//            $order->update(['total_amount' => $totalAmount]);
-//
-//            // 将下单的商品从购物车中移除
-//            $skuIds = collect($request->input('items'))->pluck('sku_id')->all();
-//            $cartService->remove($skuIds);
-//
-//            $this->dispatch(new CloseOrder($order, config('app.order_ttl')));
-//
-//            return $order;
-//        });
-//
-//        return $order;
-//    }
+
     public function store(OrderRequest $request, OrderService $orderService)
     {
         $user    = $request->user();
@@ -95,21 +40,50 @@ class OrdersController extends Controller
         return view('orders.show', ['order' => $order->load(['items.productSku', 'items.product'])]);
     }
 
-    public function received(Order $order, Request $request)
+    public function review(Order $order)
     {
         // 校验权限
         $this->authorize('own', $order);
-
-        // 判断订单的发货状态是否为已发货
-        if ($order->ship_status !== Order::SHIP_STATUS_DELIVERED) {
-            throw new InvalidRequestException('发货状态不正确');
+        // 判断是否已经支付
+        if (!$order->paid_at) {
+            throw new InvalidRequestException('该订单未支付，不可评价');
         }
-
-        // 更新发货状态为已收到
-        $order->update(['ship_status' => Order::SHIP_STATUS_RECEIVED]);
-
-        // 返回订单信息
-        return $order;
+        // 使用 load 方法加载关联数据，避免 N + 1 性能问题
+        return view('orders.review', ['order' => $order->load(['items.productSku', 'items.product'])]);
     }
+
+    public function sendReview(Order $order, SendReviewRequest $request)
+    {
+        // 校验权限
+        $this->authorize('own', $order);
+        if (!$order->paid_at) {
+            throw new InvalidRequestException('该订单未支付，不可评价');
+        }
+        // 判断是否已经评价
+        if ($order->reviewed) {
+            throw new InvalidRequestException('该订单已评价，不可重复提交');
+        }
+        $reviews = $request->input('reviews');
+        // 开启事务
+        \DB::transaction(function () use ($reviews, $order) {
+            // 遍历用户提交的数据
+            foreach ($reviews as $review) {
+                $orderItem = $order->items()->find($review['id']);
+                // 保存评分和评价
+                $orderItem->update([
+                    'rating'      => $review['rating'],
+                    'review'      => $review['review'],
+                    'reviewed_at' => Carbon::now(),
+                ]);
+            }
+            // 将订单标记为已评价
+            $order->update(['reviewed' => true]);
+            event(new OrderReviewd($order));
+        });
+
+        return redirect()->back();
+    }
+
+
 
 }
